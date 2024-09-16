c817799c34e252104da1e83fdec884b2a0d9de48
yanuar <yanuarnauval.work@gmail.com>
2024-09-16 11:13:53 +0700

    Initial Commit

---
+++ LearnBodyPoseVision/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LearnBodyPoseVision
+//
+//  Created by yanuar nauval ardian on 16/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LearnBodyPoseVision/LearnBodyPoseVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnBodyPoseVisionApp.swift
+//  LearnBodyPoseVision
+//
+//  Created by yanuar nauval ardian on 16/09/24.
+//
+
+import SwiftUI
+
+@main
+struct LearnBodyPoseVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
